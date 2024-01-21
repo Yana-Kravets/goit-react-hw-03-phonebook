78bafc4f3478b4d99c4320243c121127cc84e4e8
Yana-Kravets <108738688+Yana-Kravets@users.noreply.github.com>
2024-01-21 14:32:12 +0000

    Initial commit

--- src/components/ContactForm/ContactForm.jsx
+++ src/components/ContactForm/ContactForm.jsx
@@ -36,9 +36,6 @@ export class ContactForm extends Component {
               type="text"
               name="name"
               placeholder="Enter name"
-              // pattern="^[a-zA-Zа-яА-Я]+(([' -][a-zA-Zа-яА-Я ])?[a-zA-Zа-яА-Я]*)*$"
-              // title="Name may contain only letters, apostrophe, dash and spaces. For example Adrian, Jacob Mercer, Charles de Batz de Castelmore d'Artagnan"
-              // required
             />
             <ErrorMessage name="name" component={ErrorText} />
           </Label>
@@ -48,9 +45,6 @@ export class ContactForm extends Component {
               type="tel"
               name="number"
               placeholder="Enter phone number"
-              // pattern="\+?\d{1,4}?[-.\s]?\(?\d{1,3}?\)?[-.\s]?\d{1,4}[-.\s]?\d{1,4}[-.\s]?\d{1,9}"
-              // title="Phone number must be digits and can contain spaces, dashes, parentheses and can start with +"
-              // required
             />
             <ErrorMessage name="number" component={ErrorText} />
           </Label>
